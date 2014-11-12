adb7e5f9710b1749eb4adfe2a59519d37a8e6549
gossi <github@gos.si>
2014-11-12 22:02:47 +0100

    Made into a package

---
+++ src/TrixionaryModule.php
@@ -0,0 +1,30 @@
+<?php
+namespace gossi\trixionary;
+
+use keeko\core\module\AbstractModule;
+
+/**
+ * Trixionary API
+ * 
+ * @license MIT
+ * @author gossi
+ */
+class TrixionaryModule extends AbstractModule {
+
+	/**
+	 */
+	public function install() {
+	}
+
+	/**
+	 */
+	public function uninstall() {
+	}
+
+	/**
+	 * @param mixed $from
+	 * @param mixed $to
+	 */
+	public function update($from, $to) {
+	}
+}
